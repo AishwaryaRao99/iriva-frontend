d1e054e1c7088e7bbcc7b6dad3138fdbc2b4496f
Aishwarya Rao <aishwaryarao569@gmail.com>
2026-09-21 13:21:56 +0530

    Added vitest plugin for vite config and updated package.json and package-lock.json to include vitest dependencies.

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    coverage: {
+      reporter: ['text', 'lcov']
+    }
+  }
 });
